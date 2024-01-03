11a2d6a1aca48417126ef4c913ae77ac8df797f8
kimgdongju <unso99@gamil.com>
2024-01-03 18:29:50 +0900

    다시 주문할 시 장바구니 추가 안되는 버그 수정

--- Kiosk/src/main/java/kiosk/order/servlet/Order.java
+++ Kiosk/src/main/java/kiosk/order/servlet/Order.java
@@ -22,9 +22,7 @@ public class Order extends HttpServlet {
 		req.getSession().setAttribute("shopList", shopList);
 		String cpath = req.getContextPath();
 		
-		RequestDispatcher rd = req.getRequestDispatcher("/customer/order_menu.jsp"); //요청 전달자 객체
-				
-		rd.forward(req, resp);
+		resp.sendRedirect(cpath + "/customer/order_menu.jsp");
 	
 	}
 }
